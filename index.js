905437b39235622f441a01ba767643623542ff98
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-11 07:05:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+ 
+function writeCards(names) {
+  let recipients = [];
+  for (let i = 0; i < names.length; i++) {
+    recipients.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return recipients;
+}
+ 
+writeCards(recipients);
+
+function countDown(num) {
+  while (num > 0) {
+    console.log(num--);
+  }
+  console.log(num);
+}
