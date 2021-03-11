f0e668cf88909412b622ddacd27327ffb9f795f0
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-11 07:01:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
-const recipients = ["Lisa", "Kaitlin", "Jan"];
  
 function writeCards(recipients) {
+  let recipients = [];
   let i=0;
   while (i < recipients.length) {
     console.log(`Thank you, ${recipients[i]}, for the wonderful surprise gift!`);
