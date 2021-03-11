d4cfc04ebbebbf16787bd4b845f6aee58022526b
Roshanak Agahi <rosh.agahi@gmail.com>
2021-03-11 07:03:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solutions in this file
  
 function writeCards() {
-  let names = [];
+  let recipients = [];
   for (let i = 0; i < names.length; i++) {
     recipients.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
   }
